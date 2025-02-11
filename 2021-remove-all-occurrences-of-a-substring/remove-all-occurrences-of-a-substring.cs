ce569cad6d83aab018053a2b4c62e369ab872d10
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-02-11 15:07:28 +0700

    Time: 1 ms (100.00%) | Memory: 42 MB (88.89%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cs
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cs
@@ -2,8 +2,16 @@ public class Solution {
     public string RemoveOccurrences(string s, string part) {
         int index;
         int size = part.Length;
-        while ((index = s.IndexOf(part)) != -1) {
+        // culture-sensitive comparison:
+        // while ((index = s.IndexOf(part)) != -1) {
+        //     s = s[0..index] + s[(index + size)..];
+        // }
+        //
+
+        // Ordinal bỏ qua so sánh văn hóa như ký tự đặc biệt, chữ hoa, chữ thường...
+        while ((index = s.IndexOf(part,StringComparison.Ordinal)) != -1) {
             s = s[0..index] + s[(index + size)..];
-        }  return s;
+        }   
+        return s;
     }
 }
