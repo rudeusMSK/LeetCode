d2fcf766f928c727d3f066e566142b632b8930f6
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-02-11 14:58:34 +0700

    Time: 9 ms (79.63%) | Memory: 43.7 MB (29.63%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cs
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cs
@@ -2,7 +2,7 @@ public class Solution {
     public string RemoveOccurrences(string s, string part) {
         int index;
         while ((index = s.IndexOf(part)) != -1) {
-            s = s.Remove(index, part.Length);
+            s = s = s[0..index] + s[(index + part.Length)..];
         }
         return s;
     }
