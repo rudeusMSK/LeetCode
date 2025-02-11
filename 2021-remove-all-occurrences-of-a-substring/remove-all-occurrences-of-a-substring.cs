bd60b19aac9b7d29ab38ae60c7349489304b4b60
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-02-11 14:21:45 +0700

    Time: 10 ms (33.33%) | Memory: 43 MB (74.07%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public string RemoveOccurrences(string s, string part) {
+        while(s.Contains(part)) {
+          s = s.Remove(s.IndexOf(part), part.Length); 
+        }
+        return s;
+    }
+}
