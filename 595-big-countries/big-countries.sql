848872db3a820e0e7571a21f44dd87de391ee6d7
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-04-16 19:58:55 +0700

    Time: 536 ms (93.36%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,2 +1,3 @@
-SELECT w.name, w.population, w.area
-FROM World w WHERE area >= 3000000 OR population >= 25000000;
+SELECT name, population, area
+FROM World 
+WHERE area >= 3000000 OR population >= 25000000;
