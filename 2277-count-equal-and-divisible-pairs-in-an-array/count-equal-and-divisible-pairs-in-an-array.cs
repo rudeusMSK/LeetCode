d0814064f9886a5c50df1f3158daf3df55a1b225
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-04-21 10:10:21 +0700

    Time: 4 ms (50.85%) | Memory: 43 MB (53.03%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cs
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cs
@@ -1,14 +1,10 @@
 public class Solution {
     public int CountPairs(int[] nums, int k) {
-        int cnt = 0;
-        int n = nums.Length;
-        for(int i = 0; i < n; i++){
+        int cnt = 0, n = nums.Length;
+        for(int i = 0; i < n; i++)
             for(int j = i + 1; j < n; j++){
                 int multi = i * j;
-                if(nums[i] == nums[j] && multi % k == 0)
-                cnt++;
-            }
-        }
-        return cnt;
+                if(nums[i] == nums[j] && multi % k == 0) cnt++;
+            }   return cnt;
     }
 }
