01ad367d664217cfe58e8a0047d7ca3961a27b2e
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-01 23:02:49 +0700

    Time: 255 ms (85.41%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -16,3 +16,9 @@ ON a1.process_id = a2.process_id
 AND a1.machine_id = a2.machine_id
 AND a1.timestamp < a2.timestamp
 GROUP BY a1.machine_id;
+
+/*Ans:
+select machine_id,Convert(decimal(10,3),avg(m)) as processing_time 
+from (
+select machine_id, Sum(iif(activity_type='end',[timestamp],-[timestamp])) as m from Activity group by machine_id,process_id  ) as tbl group by machine_id 
+ */
