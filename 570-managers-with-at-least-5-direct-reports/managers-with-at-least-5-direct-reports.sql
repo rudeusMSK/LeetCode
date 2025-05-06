f84fd9d1582a8e4a62b7b15bbcbdb37fba9a2e75
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-06 10:27:37 +0700

    Time: 600 ms (80.05%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -5,3 +5,23 @@ JOIN        Employee e2
 ON          e1.id = e2.managerId
 GROUP BY    e1.id, e1.name
 HAVING      COUNT(e1.id) >= 5;
+
+/*Ans
+select 
+    e1.name
+from
+    employee e1
+    inner join
+    (select 
+        managerId,
+        count(managerId) as report
+    from 
+        employee
+    group by 
+        managerId
+    ) as e2
+    on
+    e1.id = e2.managerid
+
+where e2.report >= 5
+*/
