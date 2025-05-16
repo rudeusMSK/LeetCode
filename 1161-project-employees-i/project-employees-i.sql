0945ea6ecd48ac7e445925c04c603a22e4599e1d
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-16 19:23:48 +0700

    Time: 1248 ms (51.34%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,13 +1,13 @@
-SELECT p.project_id, ROUND(AVG(CAST(experience_years as DECIMAL(10,2))) ,2) AS average_years
-FROM Project p
-INNER JOIN Employee e
-ON p.employee_id = e.employee_id
-GROUP BY p.project_id
+-- SELECT p.project_id, ROUND(AVG(CAST(experience_years as DECIMAL(10,2))) ,2) AS average_years
+-- FROM Project p
+-- INNER JOIN Employee e
+-- ON p.employee_id = e.employee_id
+-- GROUP BY p.project_id
 
-/*ans:
+-- ---------- Ans No.1 ------------- --
+
 SELECT p.project_id, ROUND(SUM(e.experience_years) * 1.0 /COUNT(p.employee_id), 2) average_years 
 FROM Employee e
 JOIN Project p
     ON e.employee_id = p.employee_id
 GROUP BY p.project_id
-*/
