5c49fbed7d671cad95d526dc75ed8f918b072662
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-21 13:47:54 +0700

    Time: 1062 ms (35.39%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
-Select query_name, 
-round(AVG(rating*1.0/position),2) AS quality,
-Round(sum(case when rating <3 then 1 else 0 end)*100.0/count(query_name) ,2) as poor_query_percentage 
-from Queries 
-Group by query_name
-Order BY quality desc
+select 
+query_name,
+round(avg(rating*1.0/position),2) quality,
+round(sum(case when rating <3 then 1 else 0 end)*100.0/count(*),2) poor_query_percentage
+from Queries
+group by query_name
