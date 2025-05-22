7db65ee5683ce06365177d4d63ed32a64b7b30b4
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-22 21:21:11 +0700

    Time: 971 ms (86.87%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,6 +1,8 @@
-SELECT LEFT(trans_date ,7) as month,country ,count(id) trans_count ,
-  COUNT(case when state ='approved' then 1 else null end) as approved_count,
-        SUM(amount) as trans_total_amount,
-        SUM(case when state='approved' then amount else 0 end) as approved_total_amount
-FROM Transactions 
-GROUP BY LEFT(trans_date ,7),country 
+select left(convert(varchar, trans_date), 7) as month, 
+    country, 
+    count(id) as trans_count, 
+    count(case when state = 'approved' then id else Null end) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by left(convert(varchar, trans_date), 7) , country;
