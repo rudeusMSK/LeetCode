2187cd233a2fd5de75c5452167fc84a456284e39
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-25 10:40:02 +0700

    Time: 813 ms (93.04%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -16,3 +16,14 @@ SELECT
 FROM
     cte
 WHERE r = 1;
+
+/*Ans*/
+-- WITH FirstOrders AS (
+--     SELECT *,
+--            ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date ASC) AS rn
+--     FROM delivery
+-- )
+
+-- select round((cast(count(*) as decimal) / Cast((select count(customer_id) from FirstOrders WHERE rn = 1) as decimal)) * 100, 2) as immediate_percentage
+-- from FirstOrders
+-- where order_date = customer_pref_delivery_date and rn = 1
