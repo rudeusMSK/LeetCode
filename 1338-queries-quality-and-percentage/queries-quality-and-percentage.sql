3c247f736932b9f9e4ceddbcdc37e27ff461888b
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-26 20:59:15 +0700

    Time: 1118 ms (21.98%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,13 +1,13 @@
-select
-query_name,
-convert(numeric(10,2),sum((rating*1.0/position))/count(*)) quality,
-convert(numeric(10,2),sum(case when rating < 3 then 1.0 else 0.0 end)/count(query_name)*100.0) poor_query_percentage
-from Queries
-group by query_name
-
--- select 
+-- select
 -- query_name,
--- round(avg(rating*1.0/position),2) quality,
--- round(sum(case when rating <3 then 1 else 0 end)*100.0/count(*),2) poor_query_percentage
+-- convert(numeric(10,2),sum((rating*1.0/position))/count(*)) quality,
+-- convert(numeric(10,2),sum(case when rating < 3 then 1.0 else 0.0 end)/count(query_name)*100.0) poor_query_percentage
 -- from Queries
 -- group by query_name
+
+select 
+query_name,
+round(avg(rating*1.0/position),2) quality,
+round(sum(case when rating <3 then 1 else 0 end)*100.0/count(*),2) poor_query_percentage
+from Queries
+group by query_name
