23011c0da0b8b1e9c2c06cc41cd5ea2adccc54db
rudeus Grayrat (GC) <160387470+rudeusMSK@users.noreply.github.com>
2025-05-27 20:53:41 +0700

    Time: 1136 ms (76.69%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,4 +1,3 @@
-/*ans*/
 Select convert(varchar(7),trans_date, 120) AS month,
         country,
         count(state) as trans_count,
@@ -10,6 +9,7 @@ from transactions
 group by convert(varchar(7),trans_date, 120), country
 order by convert(varchar(7),trans_date, 120) asc, country desc
 
+/*ans*/
 -- select left(convert(varchar, trans_date), 7) as month, 
 --     country, 
 --     count(id) as trans_count, 
